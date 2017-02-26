6a5089415855ea41f346a3fe8b91f6f7200a8dbd
Carlos Pons <carlosponsn@outlook.com>
2017-02-26 16:43:05 +0100

    Add swashbucle package and configure valuescontroller

--- WebApiWithSwagger/Controllers/ValuesController.cs
+++ WebApiWithSwagger/Controllers/ValuesController.cs
@@ -6,10 +6,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiWithSwagger.Controllers
 {
+    /// <summary>
+    /// Values Controller
+    /// </summary>
     [Route("api/[controller]")]
+    [Produces("application/json")]
     public class ValuesController : Controller
     {
+
+        /// <summary>
+        /// Initialize a new instance of ValuesController
+        /// </summary>
+        public ValuesController()
+        {
+            
+        }
         // GET api/values
+        /// <summary>
+        /// Get a list of Values
+        /// </summary>
+        /// <returns>String list of values</returns>
         [HttpGet]
         public IEnumerable<string> Get()
         {
@@ -17,6 +33,11 @@ namespace WebApiWithSwagger.Controllers
         }
 
         // GET api/values/5
+        /// <summary>
+        /// Get a value based on an ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A value matching by id</returns>
         [HttpGet("{id}")]
         public string Get(int id)
         {
@@ -24,18 +45,31 @@ namespace WebApiWithSwagger.Controllers
         }
 
         // POST api/values
+        /// <summary>
+        /// Insert value 
+        /// </summary>
+        /// <param name="value"></param>
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
+        /// <summary>
+        /// Update values
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
         // DELETE api/values/5
+        /// <summary>
+        /// Delete values
+        /// </summary>
+        /// <param name="id"></param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- WebApiWithSwagger/Startup.cs
+++ WebApiWithSwagger/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.Swagger.Model;
 
 namespace WebApiWithSwagger
 {
@@ -27,6 +30,24 @@ namespace WebApiWithSwagger
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SingleApiVersion(new Info
+                {
+                    Title = "Testing swagger with a default Web API",
+                    Version = "v1",
+                    Description = "An API With Swagger",
+                    TermsOfService = "None"
+                });
+
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+
+                var xmlPath = Path.Combine(basePath, "WebApiWithSwagger.xml");
+                options.IncludeXmlComments(xmlPath);
+
+            });
             // Add framework services.
             services.AddMvc();
         }
@@ -34,6 +55,10 @@ namespace WebApiWithSwagger
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseSwagger();
+            app.UseSwaggerUi();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
